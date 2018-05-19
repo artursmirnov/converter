ce267a1acc868f32b51e4207832085ca72681360
Artur Smirnov <mail@artursmirnov.com>
2018-05-19 12:11:57 +0000

    Set up material ui

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import 'typeface-roboto';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,12 +10,16 @@ import registerServiceWorker from './registerServiceWorker';
 import * as reducers from './store/reducers';
 import sagaMiddleware from './store/sagaMiddleware';
 
+import theme from './themes/main';
+
 const store = createStore(combineReducers(reducers), applyMiddleware(sagaMiddleware));
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 registerServiceWorker();
