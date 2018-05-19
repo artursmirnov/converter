62e2fc97fb7c9f2d4d744ced725df381cfedcdb2
Artur Smirnov <mail@artursmirnov.com>
2018-05-19 11:51:43 +0000

    Set up redux and saga

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import 'typeface-roboto';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as reducers from './store/reducers';
+import sagaMiddleware from './store/sagaMiddleware';
+
+const store = createStore(combineReducers(reducers), applyMiddleware(sagaMiddleware));
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
