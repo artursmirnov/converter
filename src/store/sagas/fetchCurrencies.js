da5faa76504e2725dbb6ce69302e6a7adefd2c04
Artur Smirnov <mail@artursmirnov.com>
2018-05-20 07:34:41 +0000

    Fix store structure

--- src/store/sagas/fetchCurrencies.js
+++ src/store/sagas/fetchCurrencies.js
@@ -1,5 +1,4 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import _ from 'lodash';
 import Api from '../../services/Api';
 
 import { FETCH_CURRENCIES_REQUESTED } from '../actionTypes';
@@ -12,7 +11,16 @@ import {
 function* fetchCurrencies() {
   try {
     const currencies = yield call(Api.fetchCurrencies);
-    const currenciesByCode = _.keyBy(currencies, currency => currency.code);
+    const currenciesByCode = {};
+    currencies.forEach(currency => {
+      if (currenciesByCode[currency.code]) {
+        currenciesByCode[currency.code].countries.push(currency.country);
+      } else {
+        currency.countries = [ currency.country ];
+        delete currency.country;
+        currenciesByCode[currency.code] = currency;
+      }
+    });
     yield put(fetchCurrenciesSucceeded(currenciesByCode));
   } catch (error) {
     yield put(fetchCurrenciesFailed(error));
